31d30088f5c9e8994d068d3108e80cdcded555ca
matheusmorsch <82172593+matheusmorsch@users.noreply.github.com>
2021-06-04 15:25:58 -0300

    Add files via upload

---
+++ camadas/src/main/java/br/org/serratec/backend/dto/UsuarioInserirDTO.java
@@ -0,0 +1,45 @@
+package br.org.serratec.backend.dto;
+
+import br.org.serratec.backend.model.Usuario;
+
+public class UsuarioInserirDTO {
+	
+	private String nome;
+	private String email;
+	private String senha;
+	
+	public UsuarioInserirDTO() {
+	}
+
+	public UsuarioInserirDTO(Usuario usuario) {
+		super();
+		this.nome = usuario.getNome();
+		this.email = usuario.getEmail();
+		this.senha = usuario.getSenha();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
